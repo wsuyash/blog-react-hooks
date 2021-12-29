a8d9063c2f472dc62a485300ea7707bd49540c92
wsuyash <suyashcr07@gmail.com>
2021-12-29 16:23:36 +0530

    Complete Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import { firestore } from '../firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { Link } from 'react-router-dom'
+
 function Home() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const querySnapshot = getDocs(collection(firestore, 'posts'));	
+		querySnapshot.then((snapshot) => {
+			const posts = snapshot.docs.map((doc) => {
+				return {
+					id: doc.id,
+					...doc.data()
+				}	
+			});
+		console.log('posts', posts);
+		setPosts(posts);
+		})
+
+	}, []);
+
   return (
-    <div className="">
-		Home
-	</div>
+    <div className="home">
+			<h1>Tech Blog</h1>
+
+			<div id="blog-by">Suyash</div>
+
+			{posts.map((post, index) => (
+				<div className="post" key={`post-${index}`}>
+
+					<Link to={`/post/${post.id}`} >
+						<h3>{post.title}</h3>
+					</Link>
+
+					<p>{post.subTitle}</p>
+
+				</div>
+			))}
+		</div>
   );
 }
 
