7d5d79e715cec83f7532b369db3769bdcc7d152b
wsuyash <suyashcr07@gmail.com>
2021-12-29 17:17:59 +0530

    Complete PostDetail component

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,8 +1,26 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+import { firestore } from '../firebase'
+
 function PostDetail() {
+	const [post, setPost] = useState({});
+	const { postId } = useParams();
+
+	useEffect(() => {
+		const docRef = doc(firestore,'posts', postId);
+		const docSnap = getDoc(docRef);
+		docSnap.then((snapshot) => {
+			console.log(snapshot.data());
+			setPost(snapshot.data());
+		});
+		}, []);
+
   return (
-    <div className="">
-		PostDetail
-	</div>
+		<div className="post-detail">
+			<h1>{post.title}</h1>
+			<p>{post.content}</p>
+		</div>
   );
 }
 
